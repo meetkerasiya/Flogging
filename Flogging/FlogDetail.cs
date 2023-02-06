2717c1993bbf4e09e7e995cb8d3f100ac952ac21
meet kerasiya <69452272+meetkerasiya@users.noreply.github.com>
2023-02-06 11:18:56 +0530

    first commit

---
+++ Flogging/FlogDetail.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Flogging.Core
+{
+    public class FlogDetail
+    {
+        public FlogDetail() 
+        { 
+            Timestamp=DateTime.Now;
+        }
+        public DateTime Timestamp { get; private set; }
+
+        public string Message { get; set; }
+
+        //Where
+        public string Product { get; set; }
+        public string Layer { get; set; }
+        public string Location { get; set; }
+        public string Hostname { get; set; }
+
+        //Whoo
+
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public string CustomerId { get; set; }
+        public string CustomerName { get; set; }
+
+        //EVERYTHING ELSE
+
+        public long? ElapsedMilliseconds { get; set; }
+        public Exception Exception { get; set; }
+            
+        public string CorrelationId { get; set; }
+
+        public Dictionary<string, object> AdditionalInfo { get; set; }
+
+    }
+    
+}
---
+++ Flogging/Flogger.cs
@@ -0,0 +1,61 @@
+﻿using Flogging.Core;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.Configuration;
+
+namespace Flogging
+{
+    public static class Flogger
+    {
+        private static readonly ILogger _perfLogger;
+        private static readonly ILogger _usageLogger;
+        private static readonly ILogger _errorLogger;
+        private static readonly ILogger _diagnosticLogger;
+
+        static Flogger()
+        {
+            _perfLogger = new LoggerConfiguration()
+                .WriteTo.File("C:\\Users\\Meet  Kerasiya\\source\\repos\\Flogging\\Source\\perf.txt")
+                .CreateLogger();
+            _usageLogger = new LoggerConfiguration()
+                .WriteTo.File("C:\\Users\\Meet  Kerasiya\\source\\repos\\Flogging\\Source\\usage.txt")
+                .CreateLogger();
+            _errorLogger = new LoggerConfiguration()
+                .WriteTo.File("C:\\Users\\Meet  Kerasiya\\source\\repos\\Flogging\\Source\\error.txt")
+                .CreateLogger();
+            _diagnosticLogger = new LoggerConfiguration()
+                .WriteTo.File("C:\\Users\\Meet  Kerasiya\\source\\repos\\Flogging\\Source\\diagnostic.txt")
+                .CreateLogger();
+
+
+        }
+
+        public static void WritePerf(FlogDetail infoToLog)
+        {
+            _perfLogger.Write(LogEventLevel.Information, "{@FlogDetail}", infoToLog);
+
+        }
+        public static void WriteUsage(FlogDetail infoToLog)
+        {
+            _usageLogger.Write(LogEventLevel.Information, "{@FlogDetail}", infoToLog);
+
+        }
+        public static void WriteError(FlogDetail infoToLog)
+        {
+            _errorLogger.Write(LogEventLevel.Information, "{@FlogDetail}", infoToLog);
+
+        }
+        public static void WriteDiagnostic(FlogDetail infoToLog)
+        {
+            var writeDiagnostic = Convert.ToBoolean(ConfigurationManager.AppSettings["EnableDiagnostics"]);
+            if (!writeDiagnostic)
+            {
+                return;
+            }
+            _diagnosticLogger.Write(LogEventLevel.Information, "{@FlogDetail}", infoToLog);
+
+        }
+
+    }
+}
